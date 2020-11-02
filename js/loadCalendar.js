0b8891fe5489965b6e9a7a091dbd6b8a7b4017c9
Ruoyao Yang <eimee@ruoyaos-mbp.lan>
2020-11-02 13:49:02 -0600

    fix bug and speed up load calendar

--- js/loadCalendar.js
+++ js/loadCalendar.js
@@ -1,11 +1,13 @@
 // global variable
 let currentMonth;
+let daysInCurrentMonth = new Array();
 
 $(document).ready(function () {
-
     // initial calendar loading
     getCurrentMonth();
     updateCalendar(currentMonth);
+    loadEvents();
+
     $("#prev").click(function (event) {
         getPreviousMonth();
     });
@@ -14,6 +16,7 @@ $(document).ready(function () {
     });
 });
 
+// update global variable: currentMonth
 function getCurrentMonth () {
     let currentD = new Date();
     let currentY = currentD.getFullYear();
@@ -21,20 +24,26 @@ function getCurrentMonth () {
     currentMonth = new Month(currentY, currentM);
 }
 
+// update calendar with previous month
 function getPreviousMonth () {
     currentMonth = currentMonth.prevMonth();
     updateCalendar();
+    loadEvents();
 }
 
+// update calendar with next month
 function getNextMonth () {
     currentMonth = currentMonth.nextMonth();
     updateCalendar();
+    loadEvents();
 }
 
+// clear calendar grids
 function clearCalendar () {
     $("#calendar-weeks").empty();
 }
 
+// load calendar grids for a new month
 function updateCalendar () {
     // empty previous calendar
     clearCalendar();
@@ -42,7 +51,7 @@ function updateCalendar () {
     // update title info
     updateTitle();
 
-    // fill calendar with new month
+    // fill calendar with new month grids
     fillNewMonth();
 
     // highlight current date
@@ -63,43 +72,90 @@ function hightlightCurrentDate () {
     }
 }
 
-// load daily events using ajax
-function loadEvents (day) {
+// clear all events shown on current month calendar grids
+function clearEvents () {
+    for (let dayId in daysInCurrentMonth) {
+        let day = daysInCurrentMonth[dayId];
+        $("#day-" + day["month"] + "-" + day["date"]).empty();
+    }
+}
+
+// load all events shown on current month calendar grids
+function loadEvents () {
+    clearEvents();
+
+    // query current month + prev & next several days group by day
+    let data = {};
+    data["currentYear"] = currentMonth.year;
+    data["currentMonth"] = currentMonth.month + 1;
+    if (daysInCurrentMonth[0].getMonth() == currentMonth.month) {
+        data["prevMonth"] = -1;
+    }
+    else {
+        data["prevMonth"] = currentMonth.prevMonth().month + 1;
+        data["prevMonthStartDate"] = daysInCurrentMonth[0].getDate();
+    }
+    if (daysInCurrentMonth[daysInCurrentMonth.length - 1].getMonth() == currentMonth.month) {
+        data["nextMonth"] = -1;
+    }
+    else {
+        data["nextMonth"] = currentMonth.nextMonth().month + 1;
+        data["nextMonthEndDate"] = daysInCurrentMonth[daysInCurrentMonth.length - 1].getDate();
+    }
+    //console.log(data);
+
+    // day.getMonth() -- [0,11]->[Jan,Dec]
     const phpFile = "loadCalendar.php";
-    const data = { 'year': day.getFullYear(), 'month': day.getMonth(), 'date': day.getDate() };
     fetch(phpFile, {
         method: "POST",
         body: JSON.stringify(data),
         headers: { 'content-type': 'application/json' }
     })
         .then(response => response.json())
-        .then(events => displayOnHTML(day, events))
+        .then(events => displayOnHTML(events))
         .catch(err => console.error(err));
 }
 
 // display returned daily events on HTML
-function displayOnHTML (day, events) {
-    let eventFrame = document.getElementById("day-" + day.getDate());
+function displayOnHTML (events) {
+    console.log(events);
+    // load personal events
     if (events.personal_events.length != 0) {
-        for (let event_id in events.personal_events) {
-            console.log(events.personal_events[event_id].author_name);
-            let event = createPersonalEvent(events.personal_events[event_id]);
-            eventFrame.appendChild(event);
+        for (let dayId in events.personal_events) {
+            let dayEvents = events.personal_events[dayId];
+            if (dayEvents.length != 0) {
+                let eventFrame = document.getElementById("day-" + dayEvents[0].month + "-" + dayEvents[0].date);
+                for (let eventId in dayEvents) {
+                    let event = createPersonalEvent(dayEvents[eventId]);
+                    eventFrame.appendChild(event);
+                }
+            }
         }
     }
+    // load shared events
     if (events.shared_events.length != 0) {
-        for (let event_id in events.shared_events) {
-            console.log(events.personal_events[event_id].author_name);
-            let event = createSharedEvent(events.shared_events[event_id]);
-            eventFrame.appendChild(event);
+        for (let dayId in events.shared_events) {
+            let dayEvents = events.shared_events[dayId];
+            if (dayEvents.length != 0) {
+                let eventFrame = document.getElementById("day-" + dayEvents[0].month + "-" + dayEvents[0].date);
+                for (let eventId in dayEvents) {
+                    let event = createSharedEvent(dayEvents[eventId]);
+                    eventFrame.appendChild(event);
+                }
+            }
         }
     }
+    // load group events
     if (events.group_events.length != 0) {
-        for (let event_id in events.group_events) {
-            console.log(events.personal_events[event_id].author_name);
-            console.log(events.personal_events[event_id].group_name);
-            let event = createGroupEvent(events.group_events[event_id]);
-            eventFrame.appendChild(event);
+        for (let dayId in events.group_events) {
+            let dayEvents = events.group_events[dayId];
+            if (dayEvents.length != 0) {
+                let eventFrame = document.getElementById("day-" + dayEvents[0].month + "-" + dayEvents[0].date);
+                for (let eventId in dayEvents) {
+                    let event = createGroupEvent(dayEvents[eventId]);
+                    eventFrame.appendChild(event);
+                }
+            }
         }
     }
 }
@@ -129,22 +185,30 @@ function createGroupEvent (event) {
 }
 
 function fillNewMonth () {
+    // clear daysInCurrentMonth array
+    daysInCurrentMonth = [];
+
     let calendar_frame = document.getElementById("calendar-weeks");
     let weeks = currentMonth.getWeeks();
     for (let weekId in weeks) {
         let week = weeks[weekId];
+        let weekNo = Number(weekId) + 1;
         let weekElement = document.createElement("tr");
-        weekElement.setAttribute("id", "week-" + (weekId + 1));
+        weekElement.setAttribute("id", "week-" + weekNo);
         calendar_frame.appendChild(weekElement);
 
         let days = week.getDates();
-        for (var dayId in days) {
-            var day = days[dayId];
-            var date = day.getDate();
+        for (let dayId in days) {
+            let day = days[dayId];
+            let date = day.getDate();
+            let month = day.getMonth() + 1;
+            let year = day.getFullYear();
+            // update daysInCurrentMonth
+            daysInCurrentMonth.push(day);
 
             let dayElement = document.createElement("th");
             dayElement.setAttribute("class", "cell");
-            dayElement.setAttribute("id", "day-" + date);
+            dayElement.setAttribute("id", "day-" + month + "-" + date);
 
             let dayNumber = document.createElement("p");
             dayNumber.innerHTML = date;
@@ -155,9 +219,6 @@ function fillNewMonth () {
                 dayElement.setAttribute("style", "background-color: DarkGrey");
             }
             weekElement.appendChild(dayElement);
-
-            // load daily events
-            loadEvents(day);
         }
     }
 }
--- loadCalendar.php
+++ loadCalendar.php
@@ -1,27 +1,27 @@
 <?php
-    if (!isset($_SESSION)) {
-        session_start();
-        $_SESSION['user_id'] = 1;
-    }
-
     // retrieve json data from ajax
     header("Content-Type: application/json");
     // retrieve data posted via fetch()
     $json_str = file_get_contents('php://input');
     $json_obj = json_decode($json_str, true);
-    $year = $json_obj['year'];
-    $month = $json_obj['month'];
-    $date = $json_obj['date'];
+    $currentYear = $json_obj['currentYear'];
+    $currentMonth = $json_obj['currentMonth'];
+    $prevMonth = $json_obj['prevMonth'];
+    $nextMonth = $json_obj['nextMonth'];
+    if($prevMonth != -1){
+        $prevMonthStartDate = $json_obj['prevMonthStartDate'];
+    }
+    if($nextMonth != -1){
+        $nextMonthEndDate = $json_obj['nextMonthEndDate'];
+    }
 
     // TODO: verify user, sesssion start
-    // if (!$_SESSION) {
-    //     session_start();
-    // }
-    // // make sure user is logged in
-    // if (!isset($_SESSION['user_id'])) {
-    //     header("Location: index.html");
-    //     // TODO: 看是否改成json_encode success=false的形式
-    // } else {
+    session_start();
+    // make sure user is logged in
+    if (!isset($_SESSION['id'])) {
+        header("Location: index.html");
+        // TODO: 看是否改成json_encode success=false的形式
+    } else {
         $personal_events = array();
         $shared_events = array();
         $group_events = array();
@@ -29,51 +29,67 @@
         // fetch daily events by time
         // fetch personal events
         $mysqli = new mysqli('ec2-54-191-166-77.us-west-2.compute.amazonaws.com', '503', '503', 'calendar');
-        $stmt_personal = $mysqli->prepare("SELECT id, hour, minute, title, description, tag_id, author_id FROM events WHERE user_id=? AND author_id=? AND year=? AND month=? AND date=? ORDER BY hour, minute");
-        $stmt_personal->bind_param('iiiii', $_SESSION["user_id"], $_SESSION["user_id"], $year, $month, $date);
+        $stmt_personal = $mysqli->prepare("SELECT id, year, month, date, hour, minute, title, description, tag_id, author_id FROM events WHERE user_id=? AND author_id=? AND year=? AND ((month=?) OR (month=? AND date>=?) OR (month=? AND date<=?)) ORDER BY hour, minute");
+        $stmt_personal->bind_param('iiiiiiii', $_SESSION["id"], $_SESSION["id"], $currentYear, $currentMonth, $prevMonth, $prevMonthStartDate, $nextMonth, $nextMonthEndDate);
         $stmt_personal->execute();
-        $stmt_personal->bind_result($event_id, $hour, $minute, $title, $description, $tag_id, $author_id);
+        $stmt_personal->bind_result($event_id, $year, $month, $date, $hour, $minute, $title, $description, $tag_id, $author_id);
         while ($stmt_personal->fetch()) {
             // get author username
-            // $stmt_author = $mysqli->prepare("SELECT username FROM users WHERE id=?");
-            // $stmt_author->bind_param('i', $author_id);
-            // $stmt_author->execute();
-            // $stmt_author->bind_result($author_name);
-            // $stmt_author->fetch();
-            // $stmt_author->close();
-            array_push($personal_events, array(
+            $mysqli2 = new mysqli('ec2-54-191-166-77.us-west-2.compute.amazonaws.com', '503', '503', 'calendar');
+            $stmt_author = $mysqli2->prepare("SELECT username FROM users WHERE id=?");
+            $stmt_author->bind_param('i', $author_id);
+            $stmt_author->execute();
+            $stmt_author->bind_result($author_name);
+            $stmt_author->fetch();
+            $stmt_author->close();
+            $dateNo = $month+"-"+$date;
+            if(!array_key_exists($dateNo, $personal_events)){
+                $personal_events[$dateNo] = array();
+            }
+            array_push($personal_events[$dateNo], array(
                 "event_id"=>$event_id,
+                "year"=>$year,
+                "month"=>$month,
+                "date"=>$date,
                 "hour"=>$hour,
                 "minute"=>$minute,
                 "title"=>$title,
                 "description"=>$description,
                 "tag_id"=>$tag_id,
-                "author_name"=>$author_id
+                "author_name"=>$author_name
             ));
         }
         $stmt_personal->close();
 
         // fetch shared events
-        $stmt_shared = $mysqli->prepare("SELECT id, hour, minute, title, description, tag_id, author_id FROM events WHERE user_id=? AND author_id!=? AND year=? AND month=? AND date=? ORDER BY hour, minute");
-        $stmt_shared->bind_param('iiiii', $_SESSION["user_id"], $_SESSION["user_id"], $year, $month, $date);
+        $stmt_shared = $mysqli->prepare("SELECT id, year, month, date, hour, minute, title, description, tag_id, author_id FROM events WHERE user_id=? AND author_id!=? AND year=? AND ((month=?) OR (month=? AND date>=?) OR (month=? AND date<=?)) ORDER BY hour, minute");
+        $stmt_shared->bind_param('iiiiiiii', $_SESSION["id"], $_SESSION["id"], $currentYear, $currentMonth, $prevMonth, $prevMonthStartDate, $nextMonth, $nextMonthEndDate);
         $stmt_shared->execute();
-        $stmt_shared->bind_result($event_id, $hour, $minute, $title, $description, $tag_id, $author_id);
+        $stmt_shared->bind_result($event_id, $year, $month, $date, $hour, $minute, $title, $description, $tag_id, $author_id);
         while ($stmt_shared->fetch()) {
             // get author username
-            // $stmt_author = $mysqli->prepare("SELECT username FROM users WHERE id=?");
-            // $stmt_author->bind_param('i', $author_id);
-            // $stmt_author->execute();
-            // $stmt_author->bind_result($author_name);
-            // $stmt_author->fetch();
-            // $stmt_author->close();
-            array_push($shared_events, array(
+            $mysqli2 = new mysqli('ec2-54-191-166-77.us-west-2.compute.amazonaws.com', '503', '503', 'calendar');
+            $stmt_author = $mysqli2->prepare("SELECT username FROM users WHERE id=?");
+            $stmt_author->bind_param('i', $author_id);
+            $stmt_author->execute();
+            $stmt_author->bind_result($author_name);
+            $stmt_author->fetch();
+            $stmt_author->close();
+            $dateNo = $month+"-"+$date;
+            if(!array_key_exists($dateNo, $shared_events)){
+                $shared_events[$dateNo] = array();
+            }
+            array_push($shared_events[$dateNo], array(
                 "event_id"=>$event_id,
+                "year"=>$year,
+                "month"=>$month,
+                "date"=>$date,
                 "hour"=>$hour,
                 "minute"=>$minute,
                 "title"=>$title,
                 "description"=>$description,
                 "tag_id"=>$tag_id,
-                "author_name"=>$author_id
+                "author_name"=>$author_name
             ));
         }
         $stmt_shared->close();
@@ -82,7 +98,7 @@
         // get groups user belongs to
         $groups = array();
         $stmt_group1 = $mysqli->prepare("SELECT group_id from groups_users where user_id=?");
-        $stmt_group1->bind_param('i', $_SESSION["user_id"]);
+        $stmt_group1->bind_param('i', $_SESSION["id"]);
         $stmt_group1->execute();
         $stmt_group1->bind_result($group_id);
         while($stmt_group1->fetch()){
@@ -91,34 +107,42 @@
         $stmt_group1->close();
 
         foreach($groups as $group_id){
-            $stmt_group2 = $mysqli->prepare("SELECT id, hour, minute, title, description, tag_id, author_id FROM events WHERE group_id=? AND year=? AND month=? AND date=? ORDER BY hour, minute");
-            $stmt_group2->bind_param('iiii', $group_id, $year, $month, $date);
+            $stmt_group2 = $mysqli->prepare("SELECT id, year, month, date, hour, minute, title, description, tag_id, author_id FROM events WHERE group_id=? AND year=? AND ((month=?) OR (month=? AND date>=?) OR (month=? AND date<=?)) ORDER BY hour, minute");
+            $stmt_group2->bind_param('iiiiiii', $group_id, $currentYear, $currentMonth, $prevMonth, $prevMonthStartDate, $nextMonth, $nextMonthEndDate);
             $stmt_group2->execute();
-            $stmt_group2->bind_result($event_id, $hour, $minute, $title, $description, $tag_id, $author_id);
+            $stmt_group2->bind_result($event_id, $year, $month, $date, $hour, $minute, $title, $description, $tag_id, $author_id);
             while ($stmt_group2->fetch()) {
+                $mysqli2 = new mysqli('ec2-54-191-166-77.us-west-2.compute.amazonaws.com', '503', '503', 'calendar');
                 // get author username
-                // $stmt_author = $mysqli->prepare("SELECT username FROM users WHERE id=?");
-                // $stmt_author->bind_param('i', $author_id);
-                // $stmt_author->execute();
-                // $stmt_author->bind_result($author_name);
-                // $stmt_author->fetch();
-                // $stmt_author->close();
+                $stmt_author = $mysqli2->prepare("SELECT username FROM users WHERE id=?");
+                $stmt_author->bind_param('i', $author_id);
+                $stmt_author->execute();
+                $stmt_author->bind_result($author_name);
+                $stmt_author->fetch();
+                $stmt_author->close();
                 // get group name
-                // $stmt_group3 = $mysqli->prepare("SELECT name FROM groups WHERE id=?");
-                // $stmt_group3->bind_param('i', $group_id);
-                // $stmt_group3->execute();
-                // $stmt_group3->bind_result($group_name);
-                // $stmt_group3->fetch();
-                // $stmt_group3->close();
-                array_push($group_events, array(
+                $stmt_group3 = $mysqli2->prepare("SELECT name FROM groups WHERE id=?");
+                $stmt_group3->bind_param('i', $group_id);
+                $stmt_group3->execute();
+                $stmt_group3->bind_result($group_name);
+                $stmt_group3->fetch();
+                $stmt_group3->close();
+                $dateNo = $month+"-"+$date;
+                if(!array_key_exists($dateNo, $group_events)){
+                    $group_events[$dateNo] = array();
+                }
+                array_push($group_events[$dateNo], array(
                     "event_id"=>$event_id,
+                    "year"=>$year,
+                    "month"=>$month,
+                    "date"=>$date,
                     "hour"=>$hour,
                     "minute"=>$minute,
                     "title"=>$title,
                     "description"=>$description,
                     "tag_id"=>$tag_id,
-                    "author_name"=>$author_id,
-                    "group_name"=>$group_id
+                    "author_name"=>$author_name,
+                    "group_name"=>$group_name
                 ));
             }
             $stmt_group2->close();
@@ -131,5 +155,5 @@
             "shared_events"=>$shared_events,
             "group_events"=>$group_events
         ));
-    // }
+    }
 ?>
